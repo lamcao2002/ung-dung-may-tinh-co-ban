17421b569a4a022fcd8ce475e252b55ff6acc226
lamcao2002 <caovohoanglam2002@gmail.com>
2021-03-11 21:05:35 +0700

    format code

--- main.js
+++ main.js
@@ -7,21 +7,21 @@ for (let i = 0; i < classCal.length; i++) {
 
 function handler(id) {
     let num1 = Number(document.getElementById("number1").value);
-        let num2 = Number(document.getElementById("number2").value);
-        let result;
-        switch (id) {
-            case "add":
-                result = num1 + num2;
-                break;
-            case "sub":
-                result = num1 - num2;
-                break;
-            case "multi":
-                result = num1 * num2;
-                break;
-            case "div":
-                result = (num1 / num2).toFixed(2);
-                break;
-        }
-        document.getElementById("result").innerHTML = result;
+    let num2 = Number(document.getElementById("number2").value);
+    let result;
+    switch (id) {
+        case "add":
+            result = num1 + num2;
+            break;
+        case "sub":
+            result = num1 - num2;
+            break;
+        case "multi":
+            result = num1 * num2;
+            break;
+        case "div":
+            result = (num1 / num2).toFixed(2);
+            break;
+    }
+    document.getElementById("result").innerHTML = result;
 }
